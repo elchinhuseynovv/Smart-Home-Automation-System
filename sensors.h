d755b769dad61ca058f48603cb7b3fc809fb54b2
Elchin Huseynov <140554251+elchinhuseynovv@users.noreply.github.com>
2025-03-19 19:14:22 +0100

    sensors updated

--- sensors.h
+++ sensors.h
@@ -3,6 +3,8 @@
 
 #include <Arduino.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <BH1750.h>
 
 class Sensors {
 public:
@@ -12,11 +14,25 @@ public:
     float getHumidity();
     bool getMotion();
     int getLightLevel();
+    float getPreciseLightLevel(); // Precise lux measurement
+    bool isRaining();
+    float getAirQuality();
+    
+    // Averaging methods for more stable readings
+    float getAverageTemperature(int samples = 5);
+    float getAverageHumidity(int samples = 5);
     
 private:
     DHT dht;
     uint8_t pirPin;
     uint8_t ldrPin;
+    uint8_t rainPin;
+    BH1750 lightMeter;
+    unsigned long lastMotionTime;
+    bool motionState;
+    
+    // Helper methods
+    float calculateAverage(float readings[], int count);
 };
 
 #endif
